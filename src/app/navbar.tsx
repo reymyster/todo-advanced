1fcc8bb96922e29d753575c889c44f8f4db6fc7c
Rey Trajano <rey@reytrajano.com>
2023-07-02 23:14:07 -0700

    beginning layout work

---
+++ src/app/navbar.tsx
@@ -0,0 +1,21 @@
+import { Orbitron } from "next/font/google";
+
+const logoFont = Orbitron({ subsets: ["latin"], weight: ["400"] });
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <span className={`mt-2 text-xl ${logoFont.className} text-white`}>
+                To Do&apos;s
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
