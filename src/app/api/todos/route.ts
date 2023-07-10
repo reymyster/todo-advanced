5b6282eddfd62c1b650c8e52b8b6eaa58ffff601
Rey Trajano <rey@reytrajano.com>
2023-07-10 06:32:42 -0700

    consolidate api db connect logic

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,37 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
-import {
-  drizzle,
-  VercelPgClient,
-  VercelPgDatabase,
-} from "drizzle-orm/vercel-postgres";
 import { eq, asc, desc } from "drizzle-orm";
-import { db as vercel, sql } from "@vercel/postgres";
 import { todos } from "@/db/schema";
 import { ToDo, NewToDo } from "@/db/types";
 import { revalidatePath } from "next/cache";
+import { getDBSingle, getDBPool } from "@/db/utils";
 
 export const runtime = "edge";
 
-function getDB(client: VercelPgClient) {
-  let db: VercelPgDatabase<Record<string, never>>;
-
-  try {
-    db = drizzle(client);
-  } catch (err) {
-    console.error({ err });
-    try {
-      db = drizzle(client);
-    } catch (nextErr) {
-      console.error({ nextErr });
-      throw new Error("Cannot connect to database.");
-    }
-  }
-
-  return db;
-}
-
 export async function GET() {
-  const db = getDB(sql);
+  const db = getDBSingle();
 
   const allTodos = await db
     .select()
@@ -42,7 +19,7 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
-  const db = getDB(sql);
+  const db = getDBSingle();
 
   const item: NewToDo = await request.json();
 
